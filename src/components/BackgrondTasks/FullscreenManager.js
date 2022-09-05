9751ef1d47bf5c1feec6fd62fb30e6202c4b10bf
TheMarcusDev <marcushen.j@gmail.com>
2022-09-05 17:26:24 -0300

    iphone again

--- src/components/BackgrondTasks/FullscreenManager.js
+++ src/components/BackgrondTasks/FullscreenManager.js
@@ -8,21 +8,21 @@ export const FullscreenManager = () => {
     const [on, setOn] = useState(true);
 
     if (fullscreen === false) {
-        const toggleFullSceen = () => {
-            if (!document.fullscreenElement) {
-                document.documentElement.requestFullscreen();
-            } else {
-                if (document.exitFullscreen) {
-                    document.exitFullscreen();
-                }
-            }
-        };
+        // const toggleFullSceen = () => {
+        //     if (!document.fullscreenElement) {
+        //         document.documentElement.requestFullscreen();
+        //     } else {
+        //         if (document.exitFullscreen) {
+        //             document.exitFullscreen();
+        //         }
+        //     }
+        // };
         if (window.innerWidth < 450) {
             return (
                 <div className='overlayfullscreenupright'>
                     <h3 className='h3upright'>Para uma melhor experiência, ative o modo Tela Cheia:</h3>
                     <h4 className='h4upright'>(Também utilize seu celular na horizontal)</h4>
-                    <button onClick={() => { new Audio(click).play(); toggleFullSceen(); setFullscreen(true); }}>Tela Cheia</button>
+                    <button onClick={() => { new Audio(click).play(); setFullscreen(true); }}>Tela Cheia</button>
                 </div>
             )
         } else if (window.innerWidth < 950) return (
@@ -33,17 +33,19 @@ export const FullscreenManager = () => {
             </div>
         )
     }
-    // if (fullscreen === true) {
-    //     const toggleFullSceen = () => {
-    //         if (!document.fullscreenElement) {
-    //             document.documentElement.requestFullscreen();
-    //         } else {
-    //         if (document.exitFullscreen) {
-    //             document.exitFullscreen();
-    //         }
-    //     }
-    // };
-    if (window.innerWidth < 950) return (
-        <button className='btnfullscreen' onClick={() => { new Audio(click).play(); setOn(!on) }}>◰ {on ? "Desativar " : "Ativar "}Modo Tela Cheia</button>
-    )
+    if (fullscreen === true) {
+        const toggleFullSceen = () => {
+            if (!document.fullscreenElement) {
+                document.documentElement.requestFullscreen();
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                }
+            }
+        };
+        if (window.innerWidth < 950) return (
+            <button className='btnfullscreen' onClick={() => { new Audio(click).play(); toggleFullSceen(); setOn(!on) }}>◰ {on ? "Desativar " : "Ativar "}Modo Tela Cheia</button>
+        )
+    }
+
 }
