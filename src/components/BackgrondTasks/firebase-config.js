ecb596a78b0544b511d4e72088ee98ee423ea58b
Felipe Zuntini <felipezuntini@hotmail.com>
2022-11-03 23:09:54 -0300

    fix(firebase) remove of exposed credentials in favor of .env

--- src/components/BackgrondTasks/firebase-config.js
+++ src/components/BackgrondTasks/firebase-config.js
@@ -3,13 +3,13 @@ import { getFirestore } from '@firebase/firestore';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfep4PvVSmWDnqsC5MyYCfINcB6F5FkgA",
-  authDomain: "project-dandara.firebaseapp.com",
-  projectId: "project-dandara",
-  storageBucket: "project-dandara.appspot.com",
-  messagingSenderId: "943851393225",
-  appId: "1:943851393225:web:cddfe2024a8a6baf19fd18",
-  measurementId: "G-YH2SSG8RTG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
